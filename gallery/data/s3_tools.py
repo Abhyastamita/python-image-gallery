a60ad3b971947da1a5a4f6e09a384d0a6b548899
EC2 Default User <ec2-user@ip-10-10-0-241.ec2.internal>
2022-06-24 03:49:19 +0000

    Downloading images

--- gallery/data/s3_tools.py
+++ gallery/data/s3_tools.py
@@ -3,21 +3,28 @@ import boto3
 from botocore.exceptions import ClientError
 from werkzeug.utils import secure_filename
 import uuid
+import os
 
 s3_client = boto3.client('s3')
 
 def upload_image(file, owner, mime):
     img_id = str(uuid.uuid4())
     data = file
+    bucket = 'auburn.image-gallery'
     # name = secure_filename(file)
     try:
-        response = s3_client.put_object(Key=owner + "/" + img_id, Body=data, Bucket='auburn.image-gallery', ContentType=mime) # Metadata={'title': name})
+        response = s3_client.put_object(Key=owner + "/" + img_id, Body=data, Bucket=bucket, ContentType=mime) # Metadata={'title': name})
     except ClientError as e:
         logging.error(e)
         return False
     return True
 
 def download_images(owner):
-    images = s3_client.list_objects_v2(Bucket='auburn.image-gallery', Prefix=owner)
+    bucket = 'auburn.image-gallery'
+    images = s3_client.list_objects_v2(Bucket=bucket, Prefix=owner)
+    path = '/home/ec2-user/python-image-gallery/img_cache/'
+    if not os.path.exists(os.path.dirname(path + owner + "/")):
+        os.makedirs(os.path.dirname(path + owner + "/"))
     for img in images['Contents']:
-        print(img['Key'])
+        if not  os.path.isfile(path + img['Key']):
+            obj = s3_client.download_file(Bucket=bucket, Key=img['Key'], Filename=path + img['Key'])
